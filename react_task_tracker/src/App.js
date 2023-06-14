e58615ed54c59910cf07c0dede806e8ab8fe6865
zakaria <lbazriz@gmail.com>
2023-06-14 13:54:22 +0100

    make it better

---
+++ react_task_tracker/src/App.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+const App = () => {
+  const [message, setMessage] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/hello')
+      .then(response => {
+        setMessage(response.data);
+      })
+      .catch(error => {
+        if (error.message === 'Network Error') {
+          setError('Unable to connect to the server. Please ensure it is running.');
+        } else {
+          setError('An error occurred while fetching the data.');
+        }
+      });
+  }, []);
+
+  return (
+    <div>
+      {error ? (
+        <h1>Error: {error}</h1>
+      ) : (
+        <h1>{message}</h1>
+      )}
+    </div>
+  );
+};
+
+export default App;
